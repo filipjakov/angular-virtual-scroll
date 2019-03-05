5cff4eadad3bf017b1ad154b33c96ac88e670f76
fbulic <filip-jakov.bulic@fer.hr>
2019-03-05 22:09:50 +0100

    Add route to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfinteScrollComponent } from './components/infinte-scroll/infinte-scroll.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'virtual-scroll', component: InfinteScrollComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
 export class AppRoutingModule { }
